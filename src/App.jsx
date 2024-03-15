3bb645e563587224f97072bd6615ce4af9cbd0d5
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-03-15 00:55:27 -0400

    Creando metodo asincrono para obtener el usuario mediante createAsyncThunk de redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import './App.css'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { addUser } from './redux/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+// import { addUser } from './redux/userSlice'
+import { getUser } from './redux/userSlice'
 import { Header } from "./components/header"
 import { Email } from "./components/email"
 
 function App() {
 
   const dispatch = useDispatch()
-
+  const user = useSelector(state => state.user)
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users/1")
-      .then((resp) => resp.json())
-      .then((data) => dispatch(addUser(data)))
-      .catch((error) => console.log(error))
+    // fetch("https://jsonplaceholder.typicode.com/users/1")
+    //   .then((resp) => resp.json())
+    //   .then((data) => dispatch(addUser(data)))
+    //   .catch((error) => console.log(error))
+    
+    dispatch(getUser())
+
+    console.log(user)
   }, [])
 
   return (
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,24 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getUser = createAsyncThunk("getUser", async () =>{
+    try {
+        const resp = await fetch("https://jsonplaceholder.typicode.com/users/1")
+        const data = await resp.json()
+        return data
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+    
+})
 
 const initialState = {
     name: "",
     username: "",
-    email: ""
+    email: "",
+    loading: false,
+    error: null
 }
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-        addUser: (state, action) => {
-            const {name, username, email} = action.payload
-            state.name = name
-            state.username = username
-            state.email = email
-        },
+        // addUser: (state, action) => {
+        //     const {name, username, email} = action.payload
+        //     state.name = name
+        //     state.username = username
+        //     state.email = email
+        //     state.loading = false
+        //     state.error = null
+        // },
         changeEmail: (state, action) => {
             state.email = action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getUser.fulfilled, (state, action) => {
+                const {name, username, email} = action.payload
+                state.name = name
+                state.username = username
+                state.email = email
+                state.loading = false
+                state.error = null
+            })
+            .addCase(getUser.pending, (state, action) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(getUser.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
     }
 })
 
