cceadb637e1b8e8319fb62298dae7e4e013e6be1
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-03-15 01:01:34 -0400

    Eliminado useSelector del componente App porque no hacia falta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch} from 'react-redux'
 // import { addUser } from './redux/userSlice'
 import { getUser } from './redux/userSlice'
 import { Header } from "./components/header"
@@ -9,7 +9,6 @@ import { Email } from "./components/email"
 function App() {
 
   const dispatch = useDispatch()
-  const user = useSelector(state => state.user)
   useEffect(() => {
     // fetch("https://jsonplaceholder.typicode.com/users/1")
     //   .then((resp) => resp.json())
